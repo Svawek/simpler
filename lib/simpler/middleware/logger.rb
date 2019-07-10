8ae1d5002d5113d2b40b12ec27facba88825e842
svawek@ <svawek@gmail.com>
2019-07-10 23:39:42 +0300

    fix one part of logger

--- lib/simpler/middleware/logger.rb
+++ lib/simpler/middleware/logger.rb
@@ -9,9 +9,9 @@ module Simpler
     end
   
     def call(env)
-      status, headers, body = @app.call(env)
+      @status, @headers, body = @app.call(env)
       add_log(env)
-      [status, headers, body]      
+      [@status, @headers, body]      
     end
 
     private
@@ -26,51 +26,45 @@ module Simpler
   
     def add_log(env)
       controller = env["simpler.controller"]
-      response = if controller == nil
-                  'No'
+      response = if controller.nil?
+                  nil
                  else
                   controller.response
                  end
       action = env["simpler.action"]
-      @log.info add_request(env["REQUEST_METHOD"], env["PATH_INFO"])
-      @log.info add_handler(controller, action)
-      if controller == nil
-        @log.info add_parameters("No")
-      else
-        @log.info add_parameters(controller.request.params)
-      end
-      @log.info add_response(response, env["simpler.template"], controller, action)
+      str = ''
+      str = <<~LOG
+        Request: #{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}"
+        Parameters: #{controller.request.params}"
+        #{handler_info(controller, action)}
+        #{response_info(response, env["simpler.template"], controller, action)}
+      LOG
+        
+      @log.info str
     end
 
-    def add_request(method, path)
-      "Request: #{method} #{path}"
-    end
+    # def str
+    #   @log
+    # end
 
-    def add_handler(controller, action)
-      if controller == nil
+    def handler_info(controller, action)
+      if controller.nil?
         "Handler: No"
       else
-        "Handler: #{controller.name.capitalize}Controller#{action}"
+        "Handler: #{controller.name.capitalize}Controller##{action}"
       end
     end
 
-    def add_parameters(params)
-      "Parameters: #{params}"
-    end
-
-    def add_response(response, template, controller, action)
-      if response == 'No'
-        "Response: #{response}"  
-      else
-        status = response.status
-        header = response.headers['Content-Type']
+    def response_info(response, template, controller, action)
+        status = @status
+        header = @headers['Content-Type']
         path = if template == nil
                 "#{controller.name}/#{action}.html.erb"
               else
                 ''
               end
         "Response: #{status} #{header} #{path}"
-      end
+      # end
     end
   
   end
